1843de26f00d320a6e4286b2520bea103330fd6b
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-06-13 17:01:35 -0300

    Exibir valores e testando em um formulário

---
+++ 11_ExibirValores/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    $nome = "Jefferson";
+    $email = "jef32@gmail.com";
+    $senha = "123";
+
+?>
+
+<form action="">
+    <div>
+        <input type="text" value="<?= $nome ?>">
+    </div>
+    <div>
+        <input type="text" value="<?= $email ?>">
+    </div>
+    <div>
+        <input type="text" value="<?= $senha ?>">
+    </div>
+
+    <button>
+        Confirmar
+    </button>
+</form>
