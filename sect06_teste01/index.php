68e04c0628956f6b463ee8babc8bda87b4fc046f
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-06-13 15:00:42 -0300

    Teste Finalizado - Comparação de Números

---
+++ sect06_teste01/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+Comparação de Números
+Escreva uma função chamada compararNumeros que recebe dois números inteiros como parâmetros.
+
+A função deve comparar os dois números e retornar uma string indicando qual número é maior ou se são iguais.
+
+Considere as seguintes condições:
+
+Se o primeiro número for maior que o segundo número, retorne a string "O primeiro número é maior."
+
+Se o segundo número for maior que o primeiro número, retorne a string "O segundo número é maior."
+
+Se os números forem iguais, retorne a string "Os números são iguais."
+*/
+
+function compararNumeros($a, $b){
+    if($a > $b){
+        return "O primeiro número é maior.";
+    }else if($a < $b){
+        return "O segundo número é maior.";
+    }else{
+        return "Os números são iguais.";
+    }
+}
